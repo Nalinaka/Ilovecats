dc136f6b31be4ee6e1a388fc67bf596c6b5ddec3
Nalinaka <nalinaka@gmail.com>
2024-10-15 19:32:33 -0700

    Finished search functionality and connection to API for breed search

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ async function main() {
     setupSortSelector(); // Setup sorting functionality
 }
 
+// This code is to pull the cat data and display it in the filter button
+// // First you need to fetch the API to send a request to your API endpoint 
 async function fetchAnimalData() {
     try {
         const response = await fetch(url, {
@@ -73,6 +75,63 @@ function setupSortSelector() {
     });
 }
 
+
+// This next section of code is for searching breed and displaying
+// It staerts with a function - the section in HTML that has code
+// for search filter has an onChange of "onSearchCat" used here to match
+
+// I added lowercase, otherwise when entered, it will be case sensitive
+ function onSearchCat(event) {
+  const userInput = event.target.value.toLowerCase ();
+  fetchBreedData (userInput);
+}
+
+  function fetchBreedData(userInput) {
+      fetch (url, { 
+      headers: {
+      'x-api-key': api_key
+    } 
+  })
+  .then (response => {
+    if (!response.ok) {
+      throw new Error ('Breed not found');
+       }
+       return response.json();
+       // Handle and return the response is the next 
+     })
+.then(data => {
+  const breedData = data.filter(cat => cat.name.toLowerCase().includes(userInput) && cat.image);
+  renderBreed(breedData);
+})
+// Catch any errors, for example breed entered not listed
+.catch(error => {
+  console.error("Error:", error);
+});
+
+
+// Need to render the data now so it can display!  
+function renderBreed(breedData) {
+  const html = breedData.map(animal =>
+   `<div class="animal-data">
+     <h3>${animal.name}</h3>
+     <p><b>Id:</b> ${animal.id}</p>
+     <p><b>Weight:</b> ${animal.weight.metric} kg</p>
+     <p class="description"><b>Description:</b> ${animal.description || 'No description available'}</p>
+     <p class="temperament__style"><b>Temperament:</b> ${animal.temperament || 'N/A'}</p>
+     <img src="${animal.image?.url}" alt="${animal.name}">
+     <button onclick="showAnimalPosts('${animal.id}')">View More</button>
+ </div>
+`).join("");
+
+// Render the HTML
+document.getElementById('targetElement').innerHTML = html;
+}
+
+document.getElementById('searchInput').addEventListener('input', onSearchCat);
+
+window.onload = fetchCatData;
+   
+
 // Call the main function when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
     main();
@@ -82,7 +141,6 @@ function showAnimalPosts(id) {
     localStorage.setItem("id", id);
     window.location.href = `${window.location.origin}/findyourcat.html`; // Modify the page URL 
 }
-
 
 
 
